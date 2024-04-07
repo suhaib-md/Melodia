e42750e91f221bff49e3adc02a29966bfff3fbd0
Muhammed Suhaib <suhaib.muhammed2002@gmail.com>
2024-04-07 23:29:45 +0530

    made major changes

--- melodio/melodio/settings.py
+++ melodio/melodio/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -120,11 +121,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
 
 MEDIA_ROOT = BASE_DIR/'static'
+
+STATICFILES_DIRS = [
+    BASE_DIR /'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- melodio/music/admin.py
+++ melodio/music/admin.py
@@ -5,6 +5,7 @@ admin.site.register(Genre)
 admin.site.register(Artist)
 admin.site.register(Album)
 admin.site.register(Song)
-
+admin.site.register(WatchLater)
+admin.site.register(UploadedSong)
 
 
